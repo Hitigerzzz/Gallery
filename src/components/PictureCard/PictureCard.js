9f41426830196046dfd7e42333b0369d34060f81
Hitigerzzz <151250163@smail.nju.edu.cn>
2017-11-09 18:13:47 +0800

    feat:complete picturecard ui part

--- src/components/PictureCard/PictureCard.js
+++ src/components/PictureCard/PictureCard.js
@@ -2,34 +2,92 @@
  * Created by Hitigerzzz on 2017/11/8.
  */
 import React from 'react';
+import { Button } from 'antd';
 import styles from './PictureCard.css';
 import demo from '../../assets/img/wide.jpg';
 import avatar from '../../assets/img/jj.jpg';
 
-const PictureCard = () => {
-  return (
-    <div className={styles.container}>
-      <div className={styles.header}>
-        <div className={styles.avatar_wrapper}>
-          <img alt="avatar" src={avatar} />
-        </div>
-        <div className={styles.post_origin}>
-          <a className={styles.username}>Faker</a>
-          <p className={styles.post_time}>2017年10月21日 21:10</p>
-        </div>
-      </div>
-      <a className={styles.body}>
-        <img alt="post" src={demo} />
-      </a>
-      <div className={styles.footer}>
-        <div className={styles.footer_header} />
-        <div className={styles.footer_description} />
-      </div>
-    </div>
-  );
-};
+class PictureCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLike: false,
+      isFollowing: false,
+    };
+  }
 
-PictureCard.propTypes = {
-};
+  likePictureChange = () => {
+    this.setState({
+      isLike: !this.state.isLike,
+    });
+  };
+
+  /**
+   * 关注作者
+   */
+  followAuthor = () => {
+    this.setState({
+      isFollowing: true,
+    });
+  };
+  /**
+   * 取消关注
+   */
+  unfollowAuthor = () => {
+    this.setState({
+      isFollowing: false,
+    });
+  };
+
+  render() {
+    return (
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <div className={styles.avatar_wrapper}>
+            <img alt="avatar" src={avatar} />
+          </div>
+          <div className={styles.post_origin}>
+            <a className={styles.author_name}>Faker</a>
+            <p className={styles.post_time}>2017年10月21日 21:10</p>
+          </div>
+          {this.state.isFollowing ?
+            <Button className={styles.following} onClick={this.unfollowAuthor}>
+              <span className={styles.following_show}>Following</span>
+              <span className={styles.following_hide}>Unfollow</span>
+            </Button>
+            : <Button className={styles.follow} onClick={this.followAuthor}>Follow</Button>}
+        </div>
+        <a className={styles.body}>
+          <img alt="post" src={demo} />
+        </a>
+        <div className={styles.footer}>
+          <div className={styles.footer_header}>
+            <h3>Helmcken Falls sunsetHelmcken Falls sunsetHelmcken Falls sunset
+              Helmcken Falls sunsetHelmcken Falls sunsetHelmcken Falls sunset</h3>
+            <div className={styles.buttons}>
+              <Button icon="message" />
+              <Button icon="plus" />
+              <Button
+                icon="heart" onClick={this.likePictureChange}
+                className={this.state.isLike ? styles.liked : styles.disliked}
+              >250</Button>
+            </div>
+          </div>
+          <div className={styles.footer_description}>
+            Sunset at helmcken Falls. Helmcken Falls is a 141 m (463 ft) waterfall
+            on the Murtle River within Wells Gray
+            Provincial Park in British Columbia, Canada. The protection of Helmcken
+            Falls was one of the reasons for the
+            creation of Wells Gray Provincial Park in 1939.
+            Helmcken Falls is the fourth highest waterfall in Canada, measured by
+            total straight drop without a break.
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+PictureCard.propTypes = {};
 
 export default PictureCard;
