8cc1bb736f9bea16bf7405f60a7846cb59827751
Hitigerzzz <151250163@smail.nju.edu.cn>
2017-12-12 19:51:09 +0800

    feat:complete indexpage

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import { connect } from 'dva';
-import styles from './IndexPage.css';
+import styles from './page.css';
 import MainLayout from '../components/MainLayout/MainLayout';
+import IndexImage from '../assets/img/index.jpg';
 
 function IndexPage({ location }) {
   return (
     <MainLayout location={location}>
-      <div className={styles.normal}>
-        <h1 className={styles.title}>Yay! Welcome to Gallery!</h1>
-        <div className={styles.welcome} />
-        <ul className={styles.list}>
-          <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
-          <li><a href="https://github.com/dvajs/dva-docs/blob/master/v1/en-us/getting-started.md">Getting Started</a></li>
-        </ul>
+      <div className={styles.index}>
+        <img alt="index" src={IndexImage} />
+        <div className={styles.welcome}>Welcome to Gallery!</div>
       </div>
     </MainLayout>
   );
